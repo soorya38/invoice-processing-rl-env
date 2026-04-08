1afd58139cc76360638e9005afcef5d8550194cd
soorya38 <csasoorya@gmail.com>
2026-04-08 20:14:08 +0530

    feat(server): certification tag for handlers

--- server/handlers/api.go
+++ server/handlers/api.go
@@ -72,3 +72,4 @@ func HandleState(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 	json.NewEncoder(w).Encode(currentEnv.State)
 }
+// Handler certification: 1.0
